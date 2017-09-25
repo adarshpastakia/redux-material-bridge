af0470aadf5e2167a5cf466259185c9c1d328d56
Adarsh Pastakia <adarshpastakia@outlook.com>
2017-09-25 12:48:07 +0530

    Updated material-ui beta-12

--- react-shared/src/index.js
+++ react-shared/src/index.js
@@ -7,6 +7,17 @@ export * from "./material";
 import "./shared/phonelib"
 export const PhoneLib = window.PhoneLib;
 
+export const checkPhone = (v) => PhoneLib.isValid(v);
+export const checkEmail = (v) => (/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/).test(v);
 
-export const checkPhone = (v)=>PhoneLib.isValid(v);
-export const checkEmail = (v)=>(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/).test(v);
+export const logger = {
+  info: (tag, ...rest) => {
+    if (process.env.NODE_ENV !== "production")
+      console.info(tag, rest);
+    }
+  ,
+  error: (tag, ...rest) => {
+    if (process.env.NODE_ENV !== "production")
+      console.error(tag, rest);
+    }
+  }
