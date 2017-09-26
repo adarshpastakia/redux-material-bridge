f1d3245701f5fb5b621c704c387183323b05b369
Adarsh Pastakia <adarshpastakia@outlook.com>
2017-09-26 13:32:45 +0530

    Added JssProvider wrapper to MuiThemeProvider

--- react-shared/dist/material/Theme.js
+++ react-shared/dist/material/Theme.js
@@ -7,7 +7,15 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var _react = require("react");
 
@@ -27,158 +35,211 @@ var _materialUiColors = require("material-ui/colors");
 
 var _index = require("../index");
 
-exports["default"] = function (_ref) {
-  var _ref$type = _ref.type;
-  var type = _ref$type === undefined ? "light" : _ref$type;
-  var _ref$font = _ref.font;
-  var font = _ref$font === undefined ? "Roboto" : _ref$font;
-  var _ref$primary = _ref.primary;
-  var primary = _ref$primary === undefined ? _materialUiColors.blue : _ref$primary;
-  var _ref$secondary = _ref.secondary;
-  var secondary = _ref$secondary === undefined ? _materialUiColors.pink : _ref$secondary;
-  var _ref$grey = _ref.grey;
-  var grey = _ref$grey === undefined ? _materialUiColors.blueGrey : _ref$grey;
-  var children = _ref.children;
+/* eslint-disable flowtype/require-valid-file-annotation */
 
-  var palette = (0, _materialUiStylesCreatePalette2["default"])({ type: type, primary: primary, secondary: secondary, grey: grey });
+var _jss = require('jss');
 
-  _index.logger.info("Palette", palette);
+var _jssPresetDefault = require('jss-preset-default');
 
-  var typography = (0, _materialUiStylesCreateTypography2["default"])(palette, {
-    fontSize: 13,
-    fontFamily: font + ", 'Roboto', 'Helevetica Neue', 'Segoe UI', Arial, sans-serif"
-  });
-  Object.assign(typography.headline, { fontSize: 22 });
-  Object.assign(typography.title, { fontSize: 18 });
-  Object.assign(typography.subheading, { fontSize: 15 });
-  Object.assign(typography.body1, { fontSize: 13 });
-  Object.assign(typography.body2, { fontSize: 13 });
+var _jssPresetDefault2 = _interopRequireDefault(_jssPresetDefault);
 
-  var theme = (0, _materialUiStyles.createMuiTheme)({
-    type: type,
-    direction: "ltr",
-    colors: {
-      primary: palette.primary[500],
-      secondary: palette.secondary[500],
+var _reactJssLib = require('react-jss/lib');
 
-      info: "#29B6F6",
-      danger: "#E53935",
-      success: "#43A047",
-      warning: "#FFC107",
+// Configure JSS
+var jss = (0, _jss.create)((0, _jssPresetDefault2["default"])());
+jss.options.createGenerateClassName = _materialUiStyles.createGenerateClassName;
 
-      white: "#FFFFFF",
-      black: "#263238",
-
-      grayLight: "#90A4AE",
-      grayMedium: "#607D8B",
-      grayDark: "#455A64",
-
-      menuIcon: palette.primary[100],
-      menuIconHover: palette.primary[800]
-    },
-    padded: {
-      padding: 24
-    },
-    paddedH: {
-      paddingLeft: 24,
-      paddingRight: 24
-    },
-    paddedV: {
-      paddingTop: 24,
-      paddingBottom: 24
-    },
-    alignLeft: {
-      textAlign: 'left'
-    },
-    alignCenter: {
-      textAlign: 'center'
-    },
-    alignRight: {
-      textAlign: 'right'
-    },
-    icon: {
-      small: {
-        width: 16,
-        height: 16
-      },
-      medium: {
-        width: 24,
-        height: 24
-      }
-    },
-    overrides: {
-      MuiFormLabel: {
-        focused: {
-          color: palette.primary[500]
-        }
-      },
-      MuiInput: {
-        error: {
-          '&:after': {
-            height: 1
-          },
-          '&:hover:after': {
-            height: 2
-          },
-          '&$focused:after': {
-            height: 2
-          }
-        },
-        focused: {
-          '&:not($error):after': {
-            backgroundColor: palette.primary[500]
-          }
-        },
-        underline: {
-          '&:hover:not($disabled):before': {
-            height: 1
-          }
-        }
-      },
-      MuiAppBar: {
-        root: {
-          backgroundColor: palette.primary[500]
-        }
-      },
-      MuiTable: {
-        root: {
-          border: "1px solid " + palette.text.lightDivider
-        }
-      },
-      MuiCardHeader: {
-        root: {
-          minHeight: 32
-        }
-      },
-      MuiTableRow: {
-        head: {
-          height: 48,
-          backgroundColor: palette.primary[type === "dark" ? 900 : 50]
-        },
-        root: {
-          backgroundColor: palette.background.paper
-        }
-      }
-    },
-    palette: palette,
-    typography: typography
-  });
-
-  _index.logger.info("MaterialTheme", theme);
-
-  return _react2["default"].createElement(
-    "div",
-    { style: {
-        minHeight: "100vh",
-        color: palette.text.primary,
-        backgroundColor: palette.background["default"]
-      } },
-    _react2["default"].createElement(
-      _materialUiStyles.MuiThemeProvider,
-      { theme: theme },
-      children
-    )
-  );
+var context = {
+  jss: jss,
+  // This is needed in order to deduplicate the injection of CSS in the page.
+  sheetsManager: new Map(),
+  // This is needed in order to inject the critical CSS.
+  sheetsRegistry: new _reactJssLib.SheetsRegistry()
 };
+/*****       *****/
 
+var MuiTheme = (function (_Component) {
+  _inherits(MuiTheme, _Component);
+
+  function MuiTheme() {
+    _classCallCheck(this, MuiTheme);
+
+    _get(Object.getPrototypeOf(MuiTheme.prototype), "constructor", this).apply(this, arguments);
+  }
+
+  _createClass(MuiTheme, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      // Remove the server-side injected CSS.
+      var jssStyles = document.querySelector('#jss-server-side');
+      if (jssStyles && jssStyles.parentNode) {
+        jssStyles.parentNode.removeChild(jssStyles);
+      }
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var _props = this.props;
+      var _props$type = _props.type;
+      var type = _props$type === undefined ? "light" : _props$type;
+      var _props$font = _props.font;
+      var font = _props$font === undefined ? "Roboto" : _props$font;
+      var _props$primary = _props.primary;
+      var primary = _props$primary === undefined ? _materialUiColors.blue : _props$primary;
+      var _props$secondary = _props.secondary;
+      var secondary = _props$secondary === undefined ? _materialUiColors.pink : _props$secondary;
+      var _props$grey = _props.grey;
+      var grey = _props$grey === undefined ? _materialUiColors.blueGrey : _props$grey;
+      var children = _props.children;
+
+      var palette = (0, _materialUiStylesCreatePalette2["default"])({ type: type, primary: primary, secondary: secondary, grey: grey });
+
+      _index.logger.info("Palette", palette);
+
+      var typography = (0, _materialUiStylesCreateTypography2["default"])(palette, {
+        fontSize: 13,
+        fontFamily: font + ", 'Roboto', 'Helevetica Neue', 'Segoe UI', Arial, sans-serif"
+      });
+      Object.assign(typography.headline, { fontSize: 22 });
+      Object.assign(typography.title, { fontSize: 18 });
+      Object.assign(typography.subheading, { fontSize: 15 });
+      Object.assign(typography.body1, { fontSize: 13 });
+      Object.assign(typography.body2, { fontSize: 13 });
+
+      var theme = (0, _materialUiStyles.createMuiTheme)({
+        type: type,
+        direction: "ltr",
+        colors: {
+          primary: palette.primary[500],
+          secondary: palette.secondary[500],
+
+          info: "#29B6F6",
+          danger: "#E53935",
+          success: "#43A047",
+          warning: "#FFC107",
+
+          white: "#FFFFFF",
+          black: "#263238",
+
+          grayLight: "#90A4AE",
+          grayMedium: "#607D8B",
+          grayDark: "#455A64",
+
+          menuIcon: palette.primary[100],
+          menuIconHover: palette.primary[800]
+        },
+        padded: {
+          padding: 24
+        },
+        paddedH: {
+          paddingLeft: 24,
+          paddingRight: 24
+        },
+        paddedV: {
+          paddingTop: 24,
+          paddingBottom: 24
+        },
+        alignLeft: {
+          textAlign: 'left'
+        },
+        alignCenter: {
+          textAlign: 'center'
+        },
+        alignRight: {
+          textAlign: 'right'
+        },
+        icon: {
+          small: {
+            width: 16,
+            height: 16
+          },
+          medium: {
+            width: 24,
+            height: 24
+          }
+        },
+        overrides: {
+          MuiFormLabel: {
+            focused: {
+              color: palette.primary[500]
+            }
+          },
+          MuiInput: {
+            error: {
+              '&:after': {
+                height: 1
+              },
+              '&:hover:after': {
+                height: 2
+              },
+              '&$focused:after': {
+                height: 2
+              }
+            },
+            focused: {
+              '&:not($error):after': {
+                backgroundColor: palette.primary[500]
+              }
+            },
+            underline: {
+              '&:hover:not($disabled):before': {
+                height: 1
+              }
+            }
+          },
+          MuiAppBar: {
+            root: {
+              backgroundColor: palette.primary[500]
+            }
+          },
+          MuiTable: {
+            root: {
+              border: "1px solid " + palette.text.lightDivider
+            }
+          },
+          MuiCardHeader: {
+            root: {
+              minHeight: 32
+            }
+          },
+          MuiTableRow: {
+            head: {
+              height: 48,
+              backgroundColor: palette.primary[type === "dark" ? 900 : 50]
+            },
+            root: {
+              backgroundColor: palette.background.paper
+            }
+          }
+        },
+        palette: palette,
+        typography: typography
+      });
+
+      _index.logger.info("MaterialTheme", theme);
+
+      return _react2["default"].createElement(
+        "div",
+        { style: {
+            minHeight: "100vh",
+            color: palette.text.primary,
+            backgroundColor: palette.background["default"]
+          } },
+        _react2["default"].createElement(
+          _reactJssLib.JssProvider,
+          { registry: context.sheetsRegistry },
+          _react2["default"].createElement(
+            _materialUiStyles.MuiThemeProvider,
+            { theme: theme, sheetsManager: new Map() },
+            children
+          )
+        )
+      );
+    }
+  }]);
+
+  return MuiTheme;
+})(_react.Component);
+
+exports["default"] = MuiTheme;
 module.exports = exports["default"];
--- react-shared/src/material/Theme.js
+++ react-shared/src/material/Theme.js
@@ -1,156 +1,189 @@
 // @author: Adarsh Pastakia
 // Copyright © 2017, Innominds Software
 
-import React from "react";
+import React, {Component} from "react";
 import createPalette from "material-ui/styles/createPalette";
 import createTypography from "material-ui/styles/createTypography";
-import {MuiThemeProvider, createMuiTheme} from "material-ui/styles";
+import {MuiThemeProvider, createMuiTheme, createGenerateClassName} from "material-ui/styles";
 import {blue, pink, blueGrey} from "material-ui/colors";
 import {logger} from "../index";
 
-export default(({
-  type = "light",
-  font = "Roboto",
-  primary = blue,
-  secondary = pink,
-  grey = blueGrey,
-  children
-}) => {
-  const palette = createPalette({type, primary, secondary, grey});
+/* eslint-disable flowtype/require-valid-file-annotation */
+import {create} from 'jss';
+import preset from 'jss-preset-default';
+import {JssProvider, SheetsRegistry} from 'react-jss/lib';
 
-  logger.info("Palette", palette);
+// Configure JSS
+const jss = create(preset());
+jss.options.createGenerateClassName = createGenerateClassName;
 
-  const typography = createTypography(palette, {
-    fontSize: 13,
-    fontFamily: `${font}, 'Roboto', 'Helevetica Neue', 'Segoe UI', Arial, sans-serif`
-  });
-  Object.assign(typography.headline, {fontSize: 22});
-  Object.assign(typography.title, {fontSize: 18});
-  Object.assign(typography.subheading, {fontSize: 15});
-  Object.assign(typography.body1, {fontSize: 13});
-  Object.assign(typography.body2, {fontSize: 13});
+const context = {
+  jss,
+  // This is needed in order to deduplicate the injection of CSS in the page.
+  sheetsManager: new Map(),
+  // This is needed in order to inject the critical CSS.
+  sheetsRegistry: new SheetsRegistry()
+}
+/*****       *****/
 
-  const theme = createMuiTheme({
-    type: type,
-    direction: "ltr",
-    colors: {
-      primary: palette.primary[500],
-      secondary: palette.secondary[500],
+class MuiTheme extends Component {
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles
+        .parentNode
+        .removeChild(jssStyles);
+    }
+  }
+  render() {
+    const {
+      type = "light",
+      font = "Roboto",
+      primary = blue,
+      secondary = pink,
+      grey = blueGrey,
+      children
+    } = this.props;
+    const palette = createPalette({type, primary, secondary, grey});
 
-      info: "#29B6F6",
-      danger: "#E53935",
-      success: "#43A047",
-      warning: "#FFC107",
+    logger.info("Palette", palette);
 
-      white: "#FFFFFF",
-      black: "#263238",
+    const typography = createTypography(palette, {
+      fontSize: 13,
+      fontFamily: `${font}, 'Roboto', 'Helevetica Neue', 'Segoe UI', Arial, sans-serif`
+    });
+    Object.assign(typography.headline, {fontSize: 22});
+    Object.assign(typography.title, {fontSize: 18});
+    Object.assign(typography.subheading, {fontSize: 15});
+    Object.assign(typography.body1, {fontSize: 13});
+    Object.assign(typography.body2, {fontSize: 13});
 
-      grayLight: "#90A4AE",
-      grayMedium: "#607D8B",
-      grayDark: "#455A64",
+    const theme = createMuiTheme({
+      type: type,
+      direction: "ltr",
+      colors: {
+        primary: palette.primary[500],
+        secondary: palette.secondary[500],
 
-      menuIcon: palette.primary[100],
-      menuIconHover: palette.primary[800]
-    },
-    padded: {
-      padding: 24
-    },
-    paddedH: {
-      paddingLeft: 24,
-      paddingRight: 24
-    },
-    paddedV: {
-      paddingTop: 24,
-      paddingBottom: 24
-    },
-    alignLeft: {
-      textAlign: 'left'
-    },
-    alignCenter: {
-      textAlign: 'center'
-    },
-    alignRight: {
-      textAlign: 'right'
-    },
-    icon: {
-      small: {
-        width: 16,
-        height: 16
+        info: "#29B6F6",
+        danger: "#E53935",
+        success: "#43A047",
+        warning: "#FFC107",
+
+        white: "#FFFFFF",
+        black: "#263238",
+
+        grayLight: "#90A4AE",
+        grayMedium: "#607D8B",
+        grayDark: "#455A64",
+
+        menuIcon: palette.primary[100],
+        menuIconHover: palette.primary[800]
       },
-      medium: {
-        width: 24,
-        height: 24
-      }
-    },
-    overrides: {
-      MuiFormLabel: {
-        focused: {
-          color: palette.primary[500]
+      padded: {
+        padding: 24
+      },
+      paddedH: {
+        paddingLeft: 24,
+        paddingRight: 24
+      },
+      paddedV: {
+        paddingTop: 24,
+        paddingBottom: 24
+      },
+      alignLeft: {
+        textAlign: 'left'
+      },
+      alignCenter: {
+        textAlign: 'center'
+      },
+      alignRight: {
+        textAlign: 'right'
+      },
+      icon: {
+        small: {
+          width: 16,
+          height: 16
+        },
+        medium: {
+          width: 24,
+          height: 24
         }
       },
-      MuiInput: {
-        error: {
-          '&:after': {
-            height: 1
-          },
-          '&:hover:after': {
-            height: 2
-          },
-          '&$focused:after': {
-            height: 2
+      overrides: {
+        MuiFormLabel: {
+          focused: {
+            color: palette.primary[500]
           }
         },
-        focused: {
-          '&:not($error):after': {
+        MuiInput: {
+          error: {
+            '&:after': {
+              height: 1
+            },
+            '&:hover:after': {
+              height: 2
+            },
+            '&$focused:after': {
+              height: 2
+            }
+          },
+          focused: {
+            '&:not($error):after': {
+              backgroundColor: palette.primary[500]
+            }
+          },
+          underline: {
+            '&:hover:not($disabled):before': {
+              height: 1
+            }
+          }
+        },
+        MuiAppBar: {
+          root: {
             backgroundColor: palette.primary[500]
           }
         },
-        underline: {
-          '&:hover:not($disabled):before': {
-            height: 1
+        MuiTable: {
+          root: {
+            border: `1px solid ${palette.text.lightDivider}`
+          }
+        },
+        MuiCardHeader: {
+          root: {
+            minHeight: 32
+          }
+        },
+        MuiTableRow: {
+          head: {
+            height: 48,
+            backgroundColor: palette.primary[(type === "dark"
+                ? 900
+                : 50)]
+          },
+          root: {
+            backgroundColor: palette.background.paper
           }
         }
       },
-      MuiAppBar: {
-        root: {
-          backgroundColor: palette.primary[500]
-        }
-      },
-      MuiTable: {
-        root: {
-          border: `1px solid ${palette.text.lightDivider}`
-        }
-      },
-      MuiCardHeader: {
-        root: {
-          minHeight: 32
-        }
-      },
-      MuiTableRow: {
-        head: {
-          height: 48,
-          backgroundColor: palette.primary[(type === "dark"
-              ? 900
-              : 50)]
-        },
-        root: {
-          backgroundColor: palette.background.paper
-        }
-      }
-    },
-    palette,
-    typography
-  });
+      palette,
+      typography
+    });
 
-  logger.info("MaterialTheme", theme);
+    logger.info("MaterialTheme", theme);
 
-  return (
-    <div style={{
-      minHeight: "100vh",
-      color: palette.text.primary,
-      backgroundColor: palette.background.default
-    }}>
-      <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>
-    </div>
-  )
-});
+    return (
+      <div style={{
+        minHeight: "100vh",
+        color: palette.text.primary,
+        backgroundColor: palette.background.default
+      }}>
+        <JssProvider registry={context.sheetsRegistry}>
+          <MuiThemeProvider theme={theme} sheetsManager={new Map()}>{children}</MuiThemeProvider>
+        </JssProvider>
+      </div>
+    )
+  }
+}
+export default MuiTheme;
